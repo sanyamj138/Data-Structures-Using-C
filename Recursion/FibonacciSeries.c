2b0bddbe0bc84a888c024feb29713389ddf4200e
Sanyam Jain <sanyamj138@gmail.com>
2023-06-27 17:54:32 +0530

    Fibonacci Series

--- Recursion/FibonacciSeries.c
+++ Recursion/FibonacciSeries.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 
-int fib(int x) {
-    if(x == 0) {
+int fib(int x, int a, int b) {
+    if(x < 0)
         return 0;
-    }
-    if(x == 1) {
-        return 1;
-    }
-
-    return x + fib(x-1);
-}
+    printf("%d ", b);
+    return fib(x-1, b, b+a);
+}   
 
 int main() {
     int x;
 
     scanf("%d",&x);
-    printf("Sum of %d terms in Fibonacci Series: %d", x, fib(x));
+
+    printf("%d ", 0);
+    
+    fib(x - 2, 0, 1);
 
     return 0;
 }
--- Sorting/MergingSortingUsingPointer.c
+++ Sorting/MergingSortingUsingPointer.c
@@ -1,3 +1,7 @@
 // With Argument With Return
 
 #include<stdio.h>
+
+int * mergingSorting(int * arr1, int * arr2, int size1, int size2) {
+    
+}
