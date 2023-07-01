0b4302c35906ce2c577cb5e427a80fad9b1d440d
Sanyam Jain <sanyamj138@gmail.com>
2023-07-01 16:02:41 +0530

    Deletion At End

---
+++ Linked_List/Deletion/DeletionAtEnd.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct demo {
+    int data;
+    struct demo *next;
+};
+    
+    struct demo * head = NULL;
+
+void append(int data) {
+    struct demo *p = (struct demo *) malloc (sizeof(struct demo)); 
+    p->data = data;
+
+    if(head == NULL) {
+        p->next = head;
+        head = p;
+        return;
+    }
+
+    p->next = '\0';
+
+    struct demo * r = head; 
+
+    while(r->next != '\0') {
+        r = r->next;
+    }
+    r->next = p;
+}
+
+void deletionAtEnd() {
+    struct demo * p = head; 
+    
+    while(p->next->next != '\0') {
+        p = p->next;
+    }
+
+    struct demo * r = p->next;
+    p->next = '\0';
+    free(r); 
+}
+
+int main() {
+    struct demo *p;
+    int i, size;
+    int data;
+    p = (struct demo *) malloc (sizeof(struct demo));
+
+    printf("Enter the number of elements you want to enter: ");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++) {
+        scanf("%d", &data);
+        append(data);
+    }
+
+    deletionAtEnd();
+
+    p->data = head->data;
+    p->next = head->next;
+    
+    while(p != '\0') {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+
+    printf("\n");
+
+    return 0;
+}
