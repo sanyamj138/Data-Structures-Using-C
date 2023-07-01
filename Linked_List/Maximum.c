8724476f0c84bda8f8e828f414da59eed92d3644
Sanyam Jain <sanyamj138@gmail.com>
2023-07-01 15:04:27 +0530

    Maximum in a linked list

---
+++ Linked_List/Maximum.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct demo {
+    int data;
+    struct demo *next;
+};
+    
+    struct demo * head = NULL;
+
+void append(int data) {
+    struct demo *p = (struct demo *) malloc (sizeof(struct demo)); 
+    p->data = data;
+
+    if(head == NULL) {
+        p->next = head;
+        head = p;
+        return;
+    }
+
+    p->next = '\0';
+
+    struct demo * r = head; 
+
+    while(r->next != '\0') {
+        r = r->next;
+    }
+    r->next = p;
+}
+
+int max() {
+    struct demo * p = (struct demo *) malloc (sizeof(struct demo));
+    int maxi = 0;
+
+    p->data = head->data;
+    p->next = head->next;
+    
+    while(p != '\0') {
+        if(p->data > maxi) 
+            maxi = p->data;
+        p = p->next;
+    }
+
+    return maxi;
+}
+
+int main() {
+    struct demo *p;
+    int i, size;
+    int data;
+    int maxi;
+
+    p = (struct demo *) malloc (sizeof(struct demo));
+
+    printf("Enter the number of elements you want to enter: ");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++) {
+        scanf("%d", &data);
+        append(data);
+    }
+
+    maxi = max();
+
+    printf("Maximum element in the linked list is %d\n", maxi);
+
+    return 0;
+}
