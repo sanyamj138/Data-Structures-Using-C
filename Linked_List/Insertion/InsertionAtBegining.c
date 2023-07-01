432dde380ade4531216c2d755954eb0847a09e66
Sanyam Jain <sanyamj138@gmail.com>
2023-07-01 15:16:43 +0530

    Insertion At the begining in Linked List

---
+++ Linked_List/Insertion/InsertionAtBegining.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct demo {
+    int data;
+    struct demo *next;
+};
+    
+    struct demo * head = NULL;
+
+void append(int data) {
+    struct demo *p = (struct demo *) malloc (sizeof(struct demo)); 
+    p->data = data;
+
+    if(head == NULL) {
+        p->next = head;
+        head = p;
+        return;
+    }
+
+    p->next = '\0';
+
+    struct demo * r = head; 
+
+    while(r->next != '\0') {
+        r = r->next;
+    }
+    r->next = p;
+}
+
+void insertAtBegin(int data) {
+    struct demo *p = (struct demo *) malloc (sizeof(struct demo)); 
+    p->data = data;
+
+    p->next = head;
+    head = p;
+}
+
+int main() {
+    struct demo *p;
+    int i, size;
+    int data;
+    p = (struct demo *) malloc (sizeof(struct demo));
+
+    printf("Enter the number of elements you want to enter: ");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++) {
+        scanf("%d", &data);
+        append(data);
+    }
+
+    printf("Enter the number you want to add at the begining: ");
+    scanf("%d", &data);
+
+    insertAtBegin(data);
+
+    p->data = head->data;
+    p->next = head->next;
+
+    while(p != '\0') {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+
+    return 0;
+}
