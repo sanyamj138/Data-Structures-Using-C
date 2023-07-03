f8e5610ada9f3072d31000799a5807116ebea6d3
Sanyam Jain <sanyamj138@gmail.com>
2023-07-03 18:51:41 +0530

    Insertion of Doubly Linked List

---
+++ Doubly_Linked_List/Deletion/AtBegining.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node * next;
+    struct node * prev;
+};
+
+struct node * head = NULL;
+
+void append(int data) {
+    struct node * p = (struct node*) malloc (sizeof(struct node));
+
+    p->data = data;
+
+    if(head == NULL) {
+        p->next = head;
+        head = p;
+        head->prev = '\0';
+        head->next = '\0';
+        return;
+    }
+
+    struct node * r = head;
+
+    p->next = '\0';
+
+    while(r->next != '\0')
+        r = r->next;
+
+    p->prev = r;
+    r->next = p;
+}
+
+void begining() {
+    struct node * p = (struct node*) malloc (sizeof(struct node));
+    struct node * r = head;
+
+    p = head->next;
+    p->prev = '\0';
+    head = p;
+    free(r);
+}
+
+int main() {
+
+    struct node *p;
+    int i, size;
+    int data;
+    p = (struct node *) malloc (sizeof(struct node));
+
+    printf("Enter the number of elements you want to enter: ");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++) {
+        scanf("%d", &data);
+        append(data);
+    }
+
+    begining();
+
+    p->data = head->data;
+    p->next = head->next;
+
+    while(p != '\0') {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+
+    return 0;
+}
