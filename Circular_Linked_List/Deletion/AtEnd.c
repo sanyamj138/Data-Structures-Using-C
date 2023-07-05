87c9eac18b84dc1ad835d6b190a019b387991a41
Sanyam Jain <sanyamj138@gmail.com>
2023-07-05 18:32:05 +0530

    Circular Linked List Deletion At End

---
+++ Circular_Linked_List/Deletion/AtEnd.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct demo {
+    int data;
+    struct demo *next;
+};
+    
+    struct demo * head = NULL;
+
+void append(int data) {
+    struct demo *p = (struct demo *) malloc (sizeof(struct demo)); 
+    p->data = data;
+
+    if(head == NULL) {
+        p->next = head;
+        head = p;
+        p->next = head;
+        return;
+    }
+
+    p->next = head;
+
+    struct demo * r = head; 
+
+    while(r->next != head) {
+        r = r->next;
+    }
+    r->next = p;
+}
+
+void end() {
+    struct demo * r = head;
+
+    while(r->next->next != head) {
+        r = r->next;
+    }
+    r->next = head;
+}
+
+int main() {
+    struct demo *p;
+    int i, size;
+    int data;
+    p = (struct demo *) malloc (sizeof(struct demo));
+
+    printf("Enter the number of elements you want to enter: ");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++) {
+        scanf("%d", &data);
+        append(data);
+    }
+
+    end();
+
+    p = head;
+    
+    while(p->next != head) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("%d ", p->data);
+
+    return 0;
+}
