4c1a32b413df1abd511224f5de13439f83fdcb65
Sanyam Jain <sanyamj138@gmail.com>
2023-07-08 14:26:36 +0530

    Tree Program Edited

--- Tree/Creation.c
+++ Tree/Creation.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 struct node *start = NULL;
 
 struct node {
@@ -11,42 +10,34 @@ struct node {
 
 void append(int data){
     struct node * p = (struct node *) malloc (sizeof(struct node));
-    p->data = data;
 
+    p->data = data;
     if(start == NULL) {
         start = p;
         start->right = NULL;
         start->left = NULL;
         return;
-    }    
+    }
 
     struct node *root = start;
 
-    while(root->right != NULL || root->left != NULL) {
-        if(root->data > p->data) {
-            root = root->left;
-        }
-        else if(root->data < p->data) {
+    while(root != NULL) {
+        if(root->data < p->data) {
             root = root->right;
         }
+        else if(root->data > p->data) {
+            root = root->left;
+        }
     }
 
-    if(root->data < p->data) {
-        root->right = p;
-        root = root->right;
-    }
-    else {
-        root->left = p;
-        root = root->left;
-    }
-
-    p->left = NULL;
-    p->right = NULL;
+    root = p;
+    root->left = NULL;
+    root->right = NULL;
 }
 
 void display(struct node * p) {
     if(p == NULL) 
-    return;
+        return;
     display(p->left); 
     printf("%d ", p->data);
     display(p->right);
